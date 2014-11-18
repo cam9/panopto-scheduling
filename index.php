d82b95d9b211d2f1ab0fac48dcf620814d014501
luntc@bc.edu <luntc@bc.edu>
2014-11-18 15:43:01 -0500

    adds test of ldap call

--- index.php
+++ index.php
@@ -16,8 +16,44 @@
 		<input type="submit"/>
 	</form>
 	<?php
-		if (isset($_POST['username']) && !empty($_POST['username'])) {   
-			$response = file_get_contents("http://idesdev2.bc.edu/dev/ldap_jw.php?bcid=muller");
+	
+		if (isset($_POST['username']) && !empty($_POST['username'])) {  
+		$server='directory.bc.edu';
+        $admin='uid=adminides,ou=applicationadmins,dc=bc,dc=edu';
+        $passwd='rece55i0n';
+        $bcid = $_POST['username'];
+
+        $ds=ldap_connect($server);  // assuming the LDAP server is on this host
+
+        if ($ds) {
+                // bind with appropriate dn to give update access
+                $r=ldap_bind($ds, $admin, $passwd);
+                if(!$r) die("ldap_bind failed<br>");
+        } else {
+                $output = "<p>Unable to get current courses</p>";
+                return $output;
+                exit;
+        }
+
+
+        $sr = ldap_search($ds,"ou=people,dc=bc,dc=edu", "uid=$bcid", array("courseinstructorof"));
+        //$sr = ldap_search($ds,"ou=people,dc=bc,dc=edu", "uid=$bcid", array("bcismemberof"));
+
+    	$info = ldap_get_entries($ds, $sr);
+        echo "<pre>";
+        print_r($info);
+        if(!empty($info[0]['courseinstructorof'])){
+        print_r($info[0]['courseinstructorof']);
+        }else{
+                print "No courses found associated with bcid ". $bcid;
+        }
+        echo "</pre>";
+
+
+
+    	ldap_close($ds);
+     
+			$response = 
 			echo $response;
 		}
 	?>
