fb4f08ee4f1f11d77fa4ee70a5ada658e4d2ea94
luntc@bc.edu <luntc@bc.edu>
2014-11-18 16:06:53 -0500

    creates select from ldap result

--- index.php
+++ index.php
@@ -9,47 +9,52 @@
 		<input type="submit"/>
 	</form>
 	</br></br>
-	<form method="post">
-		<label for="course"> Course: </label>
-			<select id="course/"/>
-		</br></br>
-		<input type="submit"/>
-	</form>
 	<?php
 	
 		if (isset($_POST['username']) && !empty($_POST['username'])) {  
-		$server='directory.bc.edu';
-        $admin='uid=adminides,ou=applicationadmins,dc=bc,dc=edu';
-        $passwd='rece55i0n';
-        $bcid = $_POST['username'];
+			$server='directory.bc.edu';
+			$admin='uid=adminides,ou=applicationadmins,dc=bc,dc=edu';
+			$passwd='rece55i0n';
+			$bcid = $_POST['username'];
+			$ds=ldap_connect($server);  // assuming the LDAP server is on this host
 
-        $ds=ldap_connect($server);  // assuming the LDAP server is on this host
+			if ($ds) {
+					// bind with appropriate dn to give update access
+					$r=ldap_bind($ds, $admin, $passwd);
+					if(!$r) {
+						echo("<p>Unable to connect to directory.bc.edu: ldap_bind failed</p>");
+						ldap_close($ds);
+						exit();
+					}
+			} 
+			else {
+					echo "<p>Unable to connect to directory.bc.edu: ldap_connect() false</p>";
+					ldap_close($ds);
+					exit();
+			}
 
-        if ($ds) {
-                // bind with appropriate dn to give update access
-                $r=ldap_bind($ds, $admin, $passwd);
-                if(!$r) die("ldap_bind failed<br>");
-        } else {
-                $output = "<p>Unable to get current courses</p>";
-                return $output;
-                exit;
-        }
 
-
-        $sr = ldap_search($ds,"ou=people,dc=bc,dc=edu", "uid=$bcid", array("courseinstructorof"));
-        //$sr = ldap_search($ds,"ou=people,dc=bc,dc=edu", "uid=$bcid", array("bcismemberof"));
-
-    	$info = ldap_get_entries($ds, $sr);
-        echo "<pre>";
-        print_r($info);
-        if(!empty($info[0]['courseinstructorof'])){
-        print_r($info[0]['courseinstructorof']);
-        }else{
-                print "No courses found associated with bcid ". $bcid;
-        }
-        echo "</pre>";
-
-    	ldap_close($ds);
+			$sr = ldap_search($ds,"ou=people,dc=bc,dc=edu", "uid=$bcid", array("courseinstructorof"));
+			$info = ldap_get_entries($ds, $sr);
+			if(empty($info[0]['courseinstructorof'])){
+				echo("<p> No courses found associated with this bcid</p>");
+			}
+			else{
+				$courses = $info[0]['courseinstructorof'];
+				echo '<form method="post">
+						<label for="course"> Course: </label>
+						<select id="course/">';
+				for($i = 0; $i < count($courses)-1; $i+=1){
+					$course = $courses[i];
+					echo '<option value=$course>$course</option>'
+				} 
+			
+				echo '	</select>	
+						</br></br>
+						<input type="submit"/>
+					 </form>;';
+			}
+			ldap_close($ds);
 		}
 	?>
 </body>
