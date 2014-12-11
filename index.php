f0075c90b0f2a8ccb77605552786a97134482f3d
luntc@bc.edu <luntc@bc.edu>
2014-12-11 15:10:10 -0500

    route changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <body>
 <H4> Register a class to be recorded</H4>
-	<form action="/request_form.php" method="post">
+	<form action="request_form.php" method="post">
 		<label for="username"> Username: </label>
 			<input required name="username" 
 				value= "<?php if (isset($_POST['username'])) echo $_POST['username']; ?>" 
